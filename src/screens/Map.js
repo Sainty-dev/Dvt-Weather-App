46b0d6918c3ec2d2b8eb199533dd81fc38c022aa
Sainty-dev <82646950+Sainty-dev@users.noreply.github.com>
2023-07-25 05:12:39 +0200

    Added Page screens

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,14 +1,46 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import HtmlMapRenderComponent from "../components/Map";
+import WebView from "react-native-webview";
+import { getLocationAsync } from "../utils/LocationHelper";
+import useWeatherData from "../hooks/UseWeatherData";
 
-const Map = () => {
-  return (
-    <View>
-      <Text>Map</Text>
-    </View>
-  );
+const Map = ({ navigation }) => {
+  const [coordinates, setCoordinates] = useState("");
+  const { favoriteLocations, loadFavoriteLocationsFromStorage } =
+    useWeatherData();
+
+  const getCurrentLocation = async () => {
+    const coordinatesResults = await getLocationAsync();
+    setCoordinates(coordinatesResults);
+  };
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", () => {
+      getCurrentLocation();
+      loadFavoriteLocationsFromStorage();
+    });
+    return unsubscribe;
+  }, [navigation]);
+
+  const mapHtml = HtmlMapRenderComponent({
+    center: [coordinates.latitude, coordinates.longitude],
+    coordinates: { lat: coordinates.latitude, long: coordinates.longitude },
+    target: { lat: 0, long: 0 },
+    zoom: 10,
+    places: favoriteLocations,
+    from: [0, 0],
+    to: [0, 0],
+  });
+  return <WebView source={{ html: mapHtml }} style={styles.mapContainer} />;
 };
 
 export default Map;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mapContainer: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+  },
+});
