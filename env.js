d4cf24f5bef52ecbd9a1b3b311da8dbda3a84949
Sainty-dev <82646950+Sainty-dev@users.noreply.github.com>
2023-07-25 12:08:48 +0200

    Update env.js
    update

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 const ENV = {
   WEATHER_API_BASE_URL: "https://api.openweathermap.org/",
-    WEATHER_API_KEY:'dd7c020c71fc2505001ef831b6ffcb03',
+    WEATHER_API_KEY:'dd7c020c71fc2505001ef831b6ffcb03',//replace with your api key, this is for sample
     MAP_KEY:''
 
   };
